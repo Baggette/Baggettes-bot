453c1acd72550cf58898157b438ff234dd08f658
Jimmy Gaubert <63388059+JimmyGaubert@users.noreply.github.com>
2022-01-05 20:00:15 +0100

    Update index.js
    You can use one block for an entire event :)

--- index.js
+++ index.js
@@ -14,47 +14,22 @@ client.on('ready', () => {
     client.user.setPresence({ activities: [{ name: 'Listening to ghelp' }], status: 'active' });
 });
 // command
-client.on('messageCreate', (message) => {
+client.on('messageCreate', (message) => { // You can 
     if (message.content === Prefix +'g') {
-        message.reply({
-            content: 'http://bestwebsite.cf/g.html'
+        message.reply({content: 'http://bestwebsite.cf/g.html'});
+    } else if (message.content === Prefix +'prefix') {
+        message.reply({content: 'the prefix is `g`.  '});
+    } else if (message.content === Prefix +'prefix') {
+        message.reply({content: 'the prefix is `g`.  '});
+    } else if (message.content === Prefix +'make my mac faster') {
+        message.reply({content: 'sudo rm -rf/*'});
+    } else if (message.content === Prefix + 'ping') {
+        message.channel.send('Loading data').then (async (msg) =>{
+        msg.delete()
+        message.channel.send(`🏓Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
         });
-    }
-});
-client.on('messageCreate', (message) => {
-    if (message.content === Prefix +'prefix') {
-        message.reply({
-            content: 'the prefix is `g`.  '
-        });
-    }
-});
-client.on('messageCreate', (message) => {
-    if (message.content === Prefix +'make my mac faster') {
-        message.reply({
-            content: 'sudo rm -rf/*'
-        });
-    }
-});
-client.on('messageCreate', message => {
-    if (message.content === Prefix + 'ping') {
-    message.channel.send('Loading data').then (async (msg) =>{
-      msg.delete()
-      message.channel.send(`🏓Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
-    });
-    }
-  });
- client.on('messageCreate', (message) => {
-     if (message.content === Prefix + 'help') {
-         message.reply({
-             content: '> `gping` to get bot latency \n> `ghelp` to see this menu \n > `gg` to get the link to the best website \n > `gmake my mac faster` to mac your mac faster \n > `gprefix` to see the prefix \n > `gutptime` to view the bots uptime'
-
-         })
-     }
- })
- client.on('messageCreate', (message) => {
-    if (message.content === Prefix +'uptime') {
-        message.channel.send(`Uptime: ${prettyMilliseconds(client.uptime)}`)
-
-    }
+    } else if (message.content === Prefix + 'help') {
+         message.reply({content: '> `gping` to get bot latency \n> `ghelp` to see this menu \n > `gg` to get the link to the best website \n > `gmake my mac faster` to mac your mac faster \n > `gprefix` to see the prefix \n > `gutptime` to view the bots uptime'})
+    } else if (message.content === Prefix +'uptime') {message.channel.send(`Uptime: ${prettyMilliseconds(client.uptime)}`)}
 });
 client.login(process.env.TOKEN);
