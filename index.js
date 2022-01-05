ab6c53133342fe0959dcc53a9235582a90ecf3c2
Jimmy Gaubert <63388059+JimmyGaubert@users.noreply.github.com>
2022-01-05 20:14:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ client.on('ready', () => {
     client.user.setPresence({ activities: [{ name: 'Listening to ghelp' }], status: 'active' });
 });
 // command
-client.on('messageCreate', (message) => { // You can 
+client.on('messageCreate', (message) => { // You can use one block for an entire event
     if (message.content === Prefix +'g') {
         message.reply({content: 'http://bestwebsite.cf/g.html'});
     } else if (message.content === Prefix +'prefix') {
@@ -29,7 +29,7 @@ client.on('messageCreate', (message) => { // You can
         message.channel.send(`🏓Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
         });
     } else if (message.content === Prefix + 'help') {
-         message.reply({content: '> `gping` to get bot latency \n> `ghelp` to see this menu \n > `gg` to get the link to the best website \n > `gmake my mac faster` to mac your mac faster \n > `gprefix` to see the prefix \n > `gutptime` to view the bots uptime'})
+        message.reply({content: '> `gping` to get bot latency \n> `ghelp` to see this menu \n > `gg` to get the link to the best website \n > `gmake my mac faster` to mac your mac faster \n > `gprefix` to see the prefix \n > `gutptime` to view the bots uptime'})
     } else if (message.content === Prefix +'uptime') {message.channel.send(`Uptime: ${prettyMilliseconds(client.uptime)}`)}
 });
 client.login(process.env.TOKEN);
