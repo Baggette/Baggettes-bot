684e6794d0baf10a52afe59e932a7ef3858dcb5f
baggette4224 <58569691+Baggette@users.noreply.github.com>
2022-01-11 17:59:36 -0600

    Update help.js
    fixed a formatting error

--- commands/help.js
+++ commands/help.js
@@ -2,6 +2,6 @@ module.exports ={
     name: 'help',
     description: 'get a list of bot commands',
     execute(message, args){
-        message.reply({content: '> `gping` to get bot latency \n> `ghelp` to see this menu \n > `gg` to get the link to the best website \n > `gmake my mac faster` to mac your mac faster \n > `gprefix` to see the prefix \n > `gutptime` to view the bots uptime \n > `gmeme` to see a shitpost \n > `glegacy` to see a post from the r/legacy jailbreak subreddit \n > `gtwitter` to see some tweets \n > `gdog` to see a cute doggo \n `gfood` see some good food'})
+        message.reply({content: '> `gping` to get bot latency \n> `ghelp` to see this menu \n > `gg` to get the link to the best website \n > `gmake my mac faster` to mac your mac faster \n > `gprefix` to see the prefix \n > `gutptime` to view the bots uptime \n > `gmeme` to see a shitpost \n > `glegacy` to see a post from the r/legacy jailbreak subreddit \n > `gtwitter` to see some tweets \n > `gdog` to see a cute doggo \n > `gfood` see some good food'})
     }   
 }
