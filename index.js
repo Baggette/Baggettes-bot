805fe67b0fa9ac347726cd0abb3cdaf885875a7a
baggette4224 <58569691+Baggette@users.noreply.github.com>
2022-01-11 17:57:45 -0600

    Update index.js
    overhauled the command format

--- index.js
+++ index.js
@@ -1,111 +1,55 @@
 const { Client, Intents, } = require('discord.js');
 const dotenv = require('dotenv');
 dotenv.config();
-const prettyMilliseconds = require("pretty-ms");
 const got = require('got');
 const Prefix = "g"
-
+const Discord = require('discord.js');
+const fs = require('fs');
 const client = new Client({
     intents: [
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MESSAGES
     ]
 });
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command)
+}
 client.on('ready', () => {
     console.log('g is online')
     client.user.setPresence({ activities: [{ name: 'Listening to ghelp' }], status: 'active' });
 });
 // command
 client.on('messageCreate', (message) => { // You can use one block for an entire event
-    if (message.content === Prefix +'g') {
-        message.reply({content: 'http://bestwebsite.cf/g.html'});
-    } else if (message.content === Prefix +'prefix') {
-        message.reply({content: 'the prefix is `g`.  '});
-    } else if (message.content === Prefix +'prefix') {
-        message.reply({content: 'the prefix is `g`.  '});
-    } else if (message.content === Prefix +'make my mac faster') {
-        message.reply({content: 'sudo rm -rf/*'});
-    } else if (message.content === Prefix + 'ping') {
-        message.channel.send('Loading data').then (async (msg) =>{
-        msg.delete()
-        message.channel.send(`🏓Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
-        });
-    } else if (message.content === Prefix + 'help') {
-        message.reply({content: '> `gping` to get bot latency \n> `ghelp` to see this menu \n > `gg` to get the link to the best website \n > `gmake my mac faster` to mac your mac faster \n > `gprefix` to see the prefix \n > `gutptime` to view the bots uptime \n > `gmeme` to see a shitpost \n > `glegacy` to see a post from the r/legacy jailbreak subreddit \n > `gtwitter` to see some tweets'});
-    } else if (message.content === Prefix +'uptime') {message.channel.send(`Uptime: ${prettyMilliseconds(client.uptime)}`)
-    } else if (message.content === Prefix +'meme') {
-            const { MessageEmbed } = require('discord.js');
-            const embed = new MessageEmbed()
-        got('https://www.reddit.com/r/memes/random/.json')
-            .then(response => {
-                const [list] = JSON.parse(response.body);
-                const [post] = list.data.children;
-    
-                const permalink = post.data.permalink;
-                const memeUrl = `https://reddit.com${permalink}`;
-                const memeImage = post.data.url;
-                const memeTitle = post.data.title;
-                const memeUpvotes = post.data.ups;
-                const memeNumComments = post.data.num_comments;
-    
-                embed.setTitle(`${memeTitle}`);
-                embed.setURL(`${memeUrl}`);
-                embed.setColor('RANDOM');
-                embed.setImage(memeImage);
-                embed.setFooter(`👍 ${memeUpvotes} 💬 ${memeNumComments}`);
-    
-                message.channel.send({ embeds: [embed] })
-            })
-            .catch(console.error);
-    } else if (message.content === Prefix +'legacy') {
-        const { MessageEmbed } = require('discord.js');
-        const embed = new MessageEmbed()
-    got('https://www.reddit.com/r/LegacyJailbreak/random/.json')
-        .then(response => {
-            const [list] = JSON.parse(response.body);
-            const [post] = list.data.children;
-
-            const permalink = post.data.permalink;
-            const memeUrl = `https://reddit.com${permalink}`;
-            const memeImage = post.data.url;
-            const memeTitle = post.data.title;
-            const memeUpvotes = post.data.ups;
-            const memeNumComments = post.data.num_comments;
-
-            embed.setTitle(`${memeTitle}`);
-            embed.setURL(`${memeUrl}`);
-            embed.setColor('RANDOM');
-            embed.setImage(memeImage);
-            embed.setFooter(`👍 ${memeUpvotes} 💬 ${memeNumComments}`);
-
-            message.channel.send({ embeds: [embed] })
-        })
-        .catch(console.error);
-    } else if (message.content === Prefix +'twitter') {
-        const { MessageEmbed } = require('discord.js');
-        const embed = new MessageEmbed()
-    got('https://www.reddit.com/r/WhitePeopleTwitter/random/.json')
-        .then(response => {
-            const [list] = JSON.parse(response.body);
-            const [post] = list.data.children;
-
-            const permalink = post.data.permalink;
-            const memeUrl = `https://reddit.com${permalink}`;
-            const memeImage = post.data.url;
-            const memeTitle = post.data.title;
-            const memeUpvotes = post.data.ups;
-            const memeNumComments = post.data.num_comments;
-
-            embed.setTitle(`${memeTitle}`);
-            embed.setURL(`${memeUrl}`);
-            embed.setColor('RANDOM');
-            embed.setImage(memeImage);
-            embed.setFooter(`👍 ${memeUpvotes} 💬 ${memeNumComments}`);
-
-            message.channel.send({ embeds: [embed] })
-        })
-        .catch(console.error);
-};
-    
+    if (!message.content.startsWith(Prefix) || message.author.bot) return;
+    const args = message.content.slice(Prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+    if (command === 'g') {
+        client.commands.get('g').execute(message, args);
+    } else if (command ==='prefix') {
+        client.commands.get('prefix').execute(message, args);
+    } else if (command === 'make my mac faster') {
+        client.commands.get('make my mac faster').execute(message, args);
+    } else if (command === 'ping') {
+        client.commands.get('ping').execute(message, args);
+    } else if (command ==='help') {
+        client.commands.get('help').execute(message, args);
+    } else if (command === 'uptime') {
+        client.commands.get('uptime').execute(message, args);
+    } else if (command ==='meme') {
+        client.commands.get('meme').execute(message, args);
+    } else if (command ==='legacy') {
+        client.commands.get('legacy').execute(message, args);
+    } else if (command ==='twitter') {
+        client.commands.get('twitter').execute(message, args); 
+    } else if (command ==='dog') {
+        client.commands.get('dog').execute(message, args);
+    } else if (command ==='food'){
+        client.commands.get('food').execute(message, args);
+    }
 });
 client.login(process.env.TOKEN)
