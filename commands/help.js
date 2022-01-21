aea78e561009064c91d42df369092fb404727953
baggette4224 <58569691+Baggette@users.noreply.github.com>
2022-01-20 20:21:22 -0600

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,6 +2,6 @@ module.exports ={
     name: 'help',
     description: 'get a list of bot commands',
     execute(message, args){
-        message.reply({content: '> `gping` to get bot latency \n> `ghelp` to see this menu \n > `gg` to get the link to the best website \n > `gfaster` to mac your mac faster \n > `gprefix` to see the prefix \n > `gutptime` to view the bots uptime \n > `gmeme` to see a shitpost \n > `glegacy` to see a post from the r/legacy jailbreak subreddit \n > `gtwitter` to see some tweets \n > `gdog` to see a cute doggo \n > `gcat` to see a cute kitty \n > `gfood` see some good food \n > `gcar` to see some cool cars \n > `ghack <user>` to hack that user \n > `glinusface` to see a face from the linus-verse \n > `glinuslore` to see some linus lore'})
+        message.reply({content: '> `gping` to get bot latency \n> `ghelp` to see this menu \n > `gg` to get the link to the best website \n > `gfaster` to mac your mac faster \n > `gprefix` to see the prefix \n > `gutptime` to view the bots uptime \n > `gmeme` to see a shitpost \n > `glegacy` to see a post from the r/legacy jailbreak subreddit \n > `gtwitter` to see some tweets \n > `gdog` to see a cute doggo \n > `gcat` to see a cute kitty \n > `gfood` see some good food \n > `gcar` to see some cool cars \n > `ghack <user>` to hack that user \n > `glinusface` to see a face from the linus-verse \n > `glinuslore` to see some linus lore \n > `gsource` to see the bots source code'})
     }   
 }
