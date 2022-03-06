c69863d6ce0de5c554e073c29c15a0548ab5a8a1
baggette4224 <58569691+Baggette@users.noreply.github.com>
2022-03-06 15:04:12 -0600

    NEW HELP MENU
    this looks way better than what I was doing before

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,33 @@
+ const { MessageEmbed } = require('discord.js')
+
 module.exports ={
     name: 'help',
     description: 'get a list of bot commands',
     execute(message, args){
-        message.reply({content: '> `gping` to get bot latency \n> `ghelp` to see this menu \n > `gg` to get the link to the best website \n > `gfaster` to mac your mac faster \n > `gprefix` to see the prefix \n > `gutptime` to view the bots uptime \n > `gmeme` to see a shitpost \n > `glegacy` to see a post from the r/legacy jailbreak subreddit \n > `gtwitter` to see some tweets \n > `gdog` to see a cute doggo \n > `gcat` to see a cute kitty \n > `gfood` see some good food \n > `gcar` to see some cool cars \n > `ghack <user>` to hack that user \n > `glinusface` to see a face from the linus-verse \n > `glinuslore` to see some linus lore \n > `gsource` to see the bots source code'})
+        const help = new MessageEmbed
+        .setColor('#f5e942')
+        .setTitle('Baggettes Bot')
+        .setURL('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+        .setAuthor({ name: 'Baggettes Bot Help Menu', iconURL: 'https://cdn.discordapp.com/avatars/928069129892663347/2078edb679f7cc7aa97da9a6ffe84316.webp?size=80', url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'})
+        .addFields(
+            { name: '`gcar`', value: 'See a cool car'},
+            { name: '`gcat`', value: 'See a cute kitty'},
+            { name: '`gdog`', value: 'See a cute dog (dogs are better than cats imo)'},
+            { name: '`gfaster`', value: 'Make your Mac or Linux based os faster'},
+            { name: '`gfood`', value: 'This command will make you hungry'},
+            { name: '`ghack`', value: 'use this command to preform a totaly real hack on someone'},
+            { name: '`ghelp`', value: 'To see this menu'},
+            { name: '`glegacy`', value: 'Use this command to view a post from the legacy jailbreak subreddit'},
+            { name: '`glinuslore`', value: 'Use this command to see some linus lore'},
+            { name: '`glinusface`', value: 'use this command to see a hot pic from the ltt universe'},
+            { name: '`gmeme`', value: "You probably know what this is for, but if you don't it posts a meme"},
+            { name: '`gping`', value: 'Use this command to view bot latency'},
+            { name: '`gprefix`', value: 'View the bots prefix'},
+            { name: '`gsource`', value: 'View the bots source code'},
+            { name: '`gtwitter`', value: 'View a post from some idiots on twitter'},
+            { name: '`guptime`', value: 'View bot uptime'}, 
+        )
+        .setTimestamp()
+        .setFooter({ text: 'This bot was lovingly made by Baggette#4777', iconURL: 'https://cdn.discordapp.com/avatars/887756464020672523/5261d8f56ece38a54d1e88d3316310b6.jpg?size=1024'})
     }   
 }
