74d4930e4e1b12ac9006155c30e2b90ab4c12128
baggette <play.email200620062006@gmail.com>
2022-07-22 16:06:48 -0500

    checks if you're in a vc

--- commands/resume.js
+++ commands/resume.js
@@ -3,6 +3,13 @@ module.exports={
     name:"resume",
     description:"Resume a paused song",
     execute(client, message, args){
+      if (!message.member.voice.channel) {
+        const must_be_in_vc_embed = new EmbedBuilder()
+      .setColor('#f5e942')
+      .setDescription(`You must be in a voice channel!`)
+      .setTimestamp()
+        return message.channel.send({embeds:[must_be_in_vc_embed]})
+      }
         const queue = client.distube.getQueue(message)
         const no_music_embed = new EmbedBuilder()
         .setColor('#f5e942')
--- commands/seek.js
+++ commands/seek.js
@@ -3,6 +3,13 @@ module.exports={
     name:"seek",
     description:"Fast forward music",
     execute(client, message, args){
+      if (!message.member.voice.channel) {
+        const must_be_in_vc_embed = new EmbedBuilder()
+      .setColor('#f5e942')
+      .setDescription(`You must be in a voice channel!`)
+      .setTimestamp()
+        return message.channel.send({embeds:[must_be_in_vc_embed]})
+      }
         const queue = client.distube.getQueue(message)
         const no_queue_embed = new EmbedBuilder()
         .setColor('#f5e942')
