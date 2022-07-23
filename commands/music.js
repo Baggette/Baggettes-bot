af53b45bd806ddcf3ca320fae4243cded55b2767
baggette <play.email200620062006@gmail.com>
2022-07-22 19:13:48 -0500

    new command and typo fix

--- commands/music.js
+++ commands/music.js
@@ -8,17 +8,18 @@ module.exports={
         .setTitle("Baggette's bot music")
         .setDescription("See the commands for the music features of Baggettes bot")
         .addFields(
-            {name:"`p!play <song name>`", value:"Play any song you want (make sure to join a vc first)"},
-            {name:"`p!stop`", value:"Stop whatever is currently playing"},
-            {name:"`p!pause`", value:"Pause whatever is currently playing"},
-            {name:"`p!resume`", value:"Resume paused music"},
-            {name:"`p!queue`", value:"See the current music queue"},
-            {name:"`p!shuffle`", value:"Shuffle the music queue"},
-            {name:"`p!seek`", value:"Fast forward the song that's currently playing"},
-            {name:"`p!loop`", value:"Loop the current song"},
-            {name:"`p!leave`", value:"Force the bot to leave the VC"},
-            {name:"`p!nowplaying`", value:"See the name and progress of the current song"},
-            {name:"`p!skip`", value:"Skip the currently playing song"},
+            {name:"`gplay <song name>`", value:"Play any song you want (make sure to join a vc first)"},
+            {name:"`gstop`", value:"Stop whatever is currently playing"},
+            {name:"`gpause`", value:"Pause whatever is currently playing"},
+            {name:"`gresume`", value:"Resume paused music"},
+            {name:"`gqueue`", value:"See the current music queue"},
+            {name:"`gshuffle`", value:"Shuffle the music queue"},
+            {name:"`gseek`", value:"Fast forward the song that's currently playing"},
+            {name:"`gloop`", value:"Loop the current song"},
+            {name:"`gleave`", value:"Force the bot to leave the VC"},
+            {name:"`gnowplaying`", value:"See the name and progress of the current song"},
+            {name:"`gskip`", value:"Skip the currently playing song"},
+            {name:"`gvolume <0-100>`", value:"adjust the volume of the music"}
         )
         .setTimestamp()
         message.channel.send({embeds:[embed]})
