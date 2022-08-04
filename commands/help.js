ee270f8a2a83bab834155ae08edaaff3da9888e5
baggette <play.email200620062006@gmail.com>
2022-08-03 23:42:39 -0500

    fixes bug

--- commands/help.js
+++ commands/help.js
@@ -4,10 +4,12 @@ module.exports ={
     name: 'help',
     description: 'get a list of bot commands',
     execute(client, message, args){
-        const help = new EmbedBuilder()
+        if(!args[0] || args[0] === "1"){
+            const help = new EmbedBuilder()
         .setColor('#f5e942')
         .setTitle('Baggettes Bot')
         .setURL('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+        .setDescription("Use `ghelp 2` to view page two")
         .setAuthor({ name: 'Baggettes Bot Help Menu', iconURL: 'https://cdn.discordapp.com/avatars/928069129892663347/2078edb679f7cc7aa97da9a6ffe84316.webp?size=80', url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'})
         .addFields(
             { name: '`gcar`', value: 'See a cool car'},
@@ -23,6 +25,18 @@ module.exports ={
             { name: '`gmeme`', value: "You probably know what this is for, but if you don't it posts a meme"},
             { name: '`gping`', value: 'Use this command to view bot latency'},
             { name: '`gprefix`', value: 'View the bots prefix'},
+        )
+        .setTimestamp()
+        .setFooter({ text: 'This bot was lovingly made by Baggette#4777', iconURL: 'https://cdn.discordapp.com/avatars/887756464020672523/5261d8f56ece38a54d1e88d3316310b6.jpg?size=1024'})
+        message.channel.send({ embeds: [help]})
+        }else if(args[0] === "2"){
+            const help = new EmbedBuilder()
+        .setColor('#f5e942')
+        .setTitle('Baggettes Bot')
+        .setURL('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+        .setAuthor({ name: 'Baggettes Bot Help Menu', iconURL: 'https://cdn.discordapp.com/avatars/928069129892663347/2078edb679f7cc7aa97da9a6ffe84316.webp?size=80', url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'})
+        .setDescription("Use `ghelp 1` to view page one")
+        .addFields(
             { name: '`gsource`', value: 'View the bots source code'},
             { name: '`gtwitter`', value: 'View a post from some idiots on twitter'},
             { name: '`guptime`', value: 'View bot uptime'},
@@ -40,5 +54,7 @@ module.exports ={
         .setTimestamp()
         .setFooter({ text: 'This bot was lovingly made by Baggette#4777', iconURL: 'https://cdn.discordapp.com/avatars/887756464020672523/5261d8f56ece38a54d1e88d3316310b6.jpg?size=1024'})
         message.channel.send({ embeds: [help]})
+        }
+        
     }   
 }
