5f7c7d37936f13b273883044817cc882c3bac14a
Baggette <play.email200620062006@gmail.com>
2023-03-27 19:10:09 -0500

    updated some dependancies

---
+++ commands/country.js
@@ -0,0 +1,30 @@
+const got = require("got")
+const {EmbedBuilder} = require("discord.js")
+module.exports={
+    name:"country",
+    description:"See information about a country",
+    execute(client, message, args){0
+        if(!args[0]) return message.channel.send("No country provided")
+        got(`https://restcountries.com/v3.1/name/${encodeURIComponent(args.slice(0).join(" "))}?fullText=true`)
+        .then( response => {
+            const json = JSON.parse(response.body)
+            if(json.status == "404"){
+                message.channel.reply(`I was unable to find the country ${args.slice(0).join(" ")}`)
+            }else{              
+                const embed = new EmbedBuilder()
+                .setTitle(json[0].name.official)
+                .setURL(`${json[0].maps.googleMaps}`)
+                .setThumbnail(`${json[0].coatOfArms.png}`)
+                .addFields(
+                    {name:`Top level domain name`, value:`${json[0].tld[0]}`, inline: true},
+                    {name:"Independant", value:`${json[0].independent}`,},
+                    {name:"Currency", value:`${json[0].currencies.name}, Symbol ${json[0].currencies.symbol}`},
+                    {name:"Capital", value:`${json[0].capital[0]}`}, 
+                    {name:"Continent",value:`${json[0].subregion}`},
+                    {name:"Language(s)", value:`${json[0].language}`}
+                )
+                message.channel.send({embeds:[embed]})
+            }
+        })
+    }
+}
