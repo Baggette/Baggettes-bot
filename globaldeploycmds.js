abc29b9409a1e0acc98ca8cbacc57c0f0bbe3e57
muchtek <norrdinkeilan@gmail.com>
2023-04-19 19:58:44 -0600

    Added command subfolders as well as QoL change

--- globaldeploycmds.js
+++ globaldeploycmds.js
@@ -1,21 +1,34 @@
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-const dotenv = require('dotenv');
-dotenv.config();
+const { REST, Routes } = require('discord.js');
+
+require('dotenv').config();
+
 const fs = require('node:fs');
+const path = require('node:path');
 
 const commands = [];
-// Grab all the command files from the commands directory you created earlier
-const slashFiles = fs.readdirSync('./slash').filter(file => file.endsWith('.js'));
 
-// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-for (const file of slashFiles) {
-	const command = require(`./slash/${file}`);
-	commands.push(command.data.toJSON());
+const foldersPath = path.join(__dirname, 'slash');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	// Grab all the command files from the commands directory you created earlier
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if ('data' in command && 'execute' in command) {
+			commands.push(command.data.toJSON());
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
 }
 
 // Construct and prepare an instance of the REST module
-const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+const rest = new REST().setToken(process.env.TOKEN);
+console.log(process.env.TOKEN);
 // and deploy your commands!
 (async () => {
 	try {
@@ -23,7 +36,7 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 		// The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
-			Routes.applicationCommands(process.env.CLIENT_ID,),
+			Routes.applicationCommands(process.env.CLIENT_ID),
 			{ body: commands },
 		);
 
